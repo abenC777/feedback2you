297703b611d158b7f95f38840b674b70fdd6d816
Aben <alphaz1994@gmail.com>
2021-03-22 18:03:25 -0300

    added auth flow

--- index.js
+++ index.js
@@ -1,8 +1,34 @@
 const express = require("express");
-const app = express();
+const mongoose = require("mongoose");
+const cookieSession = require("cookie-session");
+const passport = require("passport");
+require("./models/User");
+require("./services/passport");
+const keys = require("./config/keys");
+
 const PORT = process.env.PORT || 5000;
-app.get("/", (req, res) => {
-  res.send({ hi: "there" });
+
+mongoose.connect(keys.mongoURI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
 });
 
+const app = express();
+
+app.use(
+  cookieSession({
+    maxAge: 30 * 24 * 60 * 1000,
+    keys: [keys.cookieKey],
+  })
+);
+app.use(passport.initialize());
+app.use(passport.session());
+
+require("./routes/authRoutes")(app);
+
 app.listen(PORT);
+
+// feedadmin
+// KNXwD5ukOtpoLpBJ
+
+// mongodb+srv://feedadmin:KNXwD5ukOtpoLpBJ@cluster0.ynlta.mongodb.net/feedback2u?retryWrites=true&w=majority
